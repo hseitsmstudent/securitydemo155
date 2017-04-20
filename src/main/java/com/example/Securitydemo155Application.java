ac09d12a723aa4f931e618c22f9a9d8fa47f79cb
hseitsmstudent <hse.itsm.student@gmail.com>
2017-04-20 15:11:14 +0300

    Initial commit

---
+++ src/main/java/com/example/Securitydemo155Application.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Securitydemo155Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Securitydemo155Application.class, args);
+	}
+}
